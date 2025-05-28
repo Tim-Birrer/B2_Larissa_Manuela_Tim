8f9cc656e0f1105b6acbac0081364038da53a7a3
Tim-Birrer <tim_birrer@sluz.ch>
2025-05-28 20:25:25 +0200

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -10,3 +10,17 @@ openBtn.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
   header.classList.remove("active");
 });
+
+/* Footerhöhe in Variable speichern für Schatten vom Footer-Bild*/
+function syncFooterHeightToImageShadow() {
+  const footer = document.querySelector("footer");
+  const footerHeight = footer.offsetHeight;
+
+  document.documentElement.style.setProperty(
+    "--footer-height",
+    `${footerHeight}px`
+  );
+}
+
+window.addEventListener("DOMContentLoaded", syncFooterHeightToImageShadow);
+window.addEventListener("resize", syncFooterHeightToImageShadow);
